492277525973d77f79adf1db9889b9bc4310ffb5
Akas <phelina94@gmail.com>
2024-04-02 15:07:08 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: '0JJ3EIGZI87FYE63386T',
+    secretAccessKey: 'pZtJp3aQJzzrZCLR0lv0LzLFFZymdPVn2e0vzbCK',
+    endpoint: 'https://s3.eu-west-2.wasabisys.com/',
+    bucket: 'esgi-cloud-exam-bucket-97171',
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
